5e2964d20a64361621ba73e6abc300829311303f
JHH <127711529+RINGO-1@users.noreply.github.com>
2024-10-25 11:43:26 +0900

    Add files via upload

--- 1018.R
+++ 1018.R
@@ -88,18 +88,40 @@ ggplot(data = df_map)+
   theme(legend.position = "none")+
   labs(x="경도", y="위도")
 
+install.packages("readxl")
+install.packages('leaflet')
 install.packages("openxlsx")
 library(openxlsx)
-install.packages("readxl")
 library(readxl)
-df = read.excel("D:/RScripts/국내지진목록.xlsx")
+library(leaflet)
+
+df = read.xlsx("D:/RScripts/국내지진목록.xlsx", sheet = 1, startRow = 4, colNames = FALSE)
 head(df)
 
 idx = grep("^북한", df$X8)
 
 df[idx, 'X8']
 
+df = df[-idx,]
+
 df[, 6] = gsub("N", "", df[, 6])
 df[, 7] = gsub("E", "", df[, 7])
 
-df[,6]
+df[,6] = as.numeric(df[,6])
+df[,7] = as.numeric(df[,7])
+
+# 행정경계지도와 지진분표 출력
+map = st_read("D:/RScripts/Z_NGII_N3A_G0010000.shp")
+
+# WGS84 좌표계
+map = st_transform(map, crs=4326)
+
+# 포인트 데이터를 sf객체로 변환(포인트 출력)
+df_sf = df%>%st_as_sf(coords = c("X7", "X6"), crs=4326)
+
+# 행정경계지도 출력
+ggplot() +
+  geom_sf(data = map, fill="white", alpha=0.5, color="black")+
+  geom_sf(data = df_sf, aes(size=X3), shape=21, fill="red", alpha=0.3, color="black")+
+  theme(legend.position = "none")+
+  labs(title = "지진분포", x = "경도", y = "위도")
